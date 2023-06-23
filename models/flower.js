a7461a022e5fd16e677e17f1d7db0c719bde42ee
bcarpenter7 <bennett.carpenter7@gmail.com>
2023-06-22 20:33:39 -0400

    styling

--- models/flower.js
+++ models/flower.js
@@ -4,8 +4,46 @@ const Schema = mongoose.Schema;
 
 const flowerSchema = new Schema({
     name: String,
-    amount: Number,
-    zone: Number
+    dateOfWedding: Date,
+    flowerOneName: String,
+    flowerOneAmount: Number,
+    greensOneName: String,
+    greensOneAmount: Number,
 }, {timestamps: true});
 
 module.exports = mongoose.model('Flower', flowerSchema)
+
+// orderDate: Date
+//flowerTwoName: String,
+//flowerTwoAmount: Number,
+//flowerThreeName: String,
+//flowerThreeAmount: Number,
+
+
+// Rose #12
+// Greenery 
+// Main flower Rose #12
+// Accent flower 
+// Name of Client
+// Order Date
+// Wedding Date
+
+// Bridal Bouquet
+// 40-50 stems
+// 12-15 types of flower
+// Flower #1
+// Flower #2
+// Flower #3
+// Flower #4
+// Greenery max would be 4
+
+// Bridesmaids bouquet
+// 30 stems
+// 12 - 15
+// 
+// Greenery max would be 4
+
+// Centerpieces
+// 5inch
+// 25 / 30 stems
+// 12-15 varieties
